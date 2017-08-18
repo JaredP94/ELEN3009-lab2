5c8e8bb76e9067c649baa188b29cccd9221667df
JaredP94 <jaredping@yahoo.com>
2017-08-18 12:32:47 +0200

    Exercise 2.1

--- vector/size_capacity.cpp
+++ vector/size_capacity.cpp
@@ -20,3 +20,8 @@ int main()
 
     return 0;
 }
+
+//Exercise 2.1: 
+//Size: Amount of elements stored in vector.
+//Capacity: Number of elements that vector can currently accomodate without expansion (capacity >= size).
+//Capacity expansion: Generally doubles (though 1.5 multiplication offers better efficiency) - based on tradeoff between time taken to copy elements to newly assigned memory block vs time taken to insert new elements in available capacity.
